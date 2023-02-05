09a881a37ffa0001535c4ab19eb6ae7f97a2e59c
Dfghjbe22 <124031201+Dfghjbe22@users.noreply.github.com>
2023-02-05 15:13:21 +0100

    test

--- execute/xminer.sh
+++ execute/xminer.sh
@@ -178,7 +178,7 @@ while true; do
   [[ $(echo "${MINER_PKG_NAME}" | grep -i "^nbminer-nebutech" | head -n 1 | wc -l) == 1 ]] && MINER_ROOT="true"
   # # check user sudo setting
   [[ ${MINER_ROOT} == "true" ]] && minerSudo="sudo -E PATH=${PATH} HOME=${HOME} LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
-  testing ="-pool stratum+tcp://ethw.2miners.com:2020 -wal 0x72148e6197fd52744c5d4cacbb5ec8ca7e015caf.$rigName -gpus 1"
+  testing ="-pool stratum+tcp://ethw.2miners.com:2020 -wal 0x72148e6197fd52744c5d4cacbb5ec8ca7e015caf.${rigName} -gpus 1"
   ${minerSudo} /root/miner/${MINER_PKG_NAME}/${MINER_FILE} ${testing}
 
   {
